0654844129a09356a82c2457d875dce6ec1fc185
Pedro Duarte <pedro.duarte@toroinvestimentos.com.br>
2020-09-27 19:42:58 -0300

    Add bootstrap

--- crud/frontend/src/components/template/Footer.jsx
+++ crud/frontend/src/components/template/Footer.jsx
@@ -5,6 +5,6 @@ export default props =>
     <footer className="footer">
         <span>
             Desenvolvido com <i className="fa fa-heart text-danger"></i> por
-            <strong> Cod<span className="text-danger">3</span>r</strong>
+            <strong> Pedro Duarte</strong>
         </span>
     </footer>
